9a7ff132060d7d62aaac4607be9e1f5acd8f2ebc
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-04-08 22:57:48 +0700

    modif: style component

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 export default function Button({ children, classname }: ButtonProps) {
   return (
     <button
-      className={`h-10 px-6 font-semibold rounded-full bg-primary text-white ${classname}`}
+      className={`h-10 px-6 font-semibold rounded-full bg-primary text-white ${classname} hover:bg-opacity-90`}
     >
       {children}
     </button>
