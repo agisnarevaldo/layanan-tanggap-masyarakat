ea5cbe549f0990b552f14ff94eefeb8495b4b170
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-04-08 22:59:45 +0700

    style: navbar sticky & blur background

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center px-20 py-2">
+    <div className="flex justify-between items-center px-20 py-2 sticky top-0 z-10 backdrop-blur-lg">
       <Image src="/ltm.svg" alt="ltm logo" width={72} height={72} />
       <ul className="items-center flex gap-10">
         <Link href="/">Home</Link>
-        <Link href="/">Tentang</Link>
-        <Link href="/">Cara Melapor</Link>
-        <Link href="/">Kontak Kami</Link>
-        <button className="h-10 px-6 font-semibold rounded-full bg-primary text-white">
+        <Link href="#about">Tentang</Link>
+        <Link href="#cara">Cara Melapor</Link>
+        <Link href="#contact">Kontak Kami</Link>
+        <button className="h-10 px-6 font-semibold rounded-full bg-primary text-white hover:bg-opacity-90">
           Login
         </button>
       </ul>
