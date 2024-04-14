100898cbc6fbe4477ee6c4b542c08269caec0b9c
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-04-14 20:07:33 +0700

    modify: login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import FormLogin from "../ui/formLogin";
 import Link from "next/link";
+import Logo from "../ui/logo";
 
 const LoginPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <Logo />
+          <h2 className="mt-6 text-center text-3xl font-bold text-color">
             Masuk ke Akun Anda
           </h2>
         </div>
