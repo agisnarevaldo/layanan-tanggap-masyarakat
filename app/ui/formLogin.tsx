d2ede39c9472627a1d575795aa11900024b79893
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-04-14 20:06:45 +0700

    modify: formLogin

--- app/ui/formLogin.tsx
+++ app/ui/formLogin.tsx
@@ -1,12 +1,33 @@
 import Button from "./button";
 import Input from "./input";
+import Label from "./label";
 
 const FormLogin = () => {
   return (
-    <form action="" className="flex flex-col gap-4">
-      <Input type="email" placeholder="Email" />
-      <Input type="password" placeholder="Password" />
-      <Button type="submit" classname="w-2/4 m-auto">
+    <form
+      action=""
+      className="flex flex-col gap-4 w-max m-auto"
+      autoComplete="on"
+    >
+      <div className="flex flex-col gap-4 items-center bg-primary py-6 rounded-2xl shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="Email"
+          className="w-3/4 m-auto"
+          autoComplete="email"
+        />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          id="password"
+          type="password"
+          placeholder="Password"
+          className="w-3/4 m-auto"
+          autoComplete="new-password"
+        />
+      </div>
+      <Button type="submit" classname="w-1/3 m-auto">
         Login
       </Button>
     </form>
