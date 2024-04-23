eaf88173ad17141c969f0becd10c8bc88facd6b3
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-04-23 22:25:46 +0700

    style: animate Link

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
+import style from "@/app/ui/home/navbar.module.css";
 
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center px-20 py-2 sticky top-0 z-10 backdrop-blur-lg">
       <Image src="/ltm.svg" alt="ltm logo" width={72} height={72} />
       <ul className="items-center flex gap-10">
-        <Link href="/">Home</Link>
-        <Link href="#about">Tentang</Link>
-        <Link href="#cara">Cara Melapor</Link>
-        <Link href="#contact">Kontak Kami</Link>
+        <Link href="/" className={style.link}>
+          Home
+        </Link>
+        <Link href="#about" className={style.link}>
+          Tentang
+        </Link>
+        <Link href="#cara" className={style.link}>
+          Cara Melapor
+        </Link>
+        <Link href="#contact" className={style.link}>
+          Kontak Kami
+        </Link>
         <Link href="/sign-in">
           <button className="h-10 px-6 font-semibold rounded-full bg-primary text-white hover:bg-opacity-90">
             Login
