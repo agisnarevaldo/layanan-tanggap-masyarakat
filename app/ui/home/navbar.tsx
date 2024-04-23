e269212ffa19221d4f6ae478896b748df5024218
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-04-23 22:28:30 +0700

    style: animate Link in home page

--- app/ui/home/navbar.tsx
+++ app/ui/home/navbar.tsx
@@ -1,17 +1,28 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import style from "./navbar.module.css";
 
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center px-20 py-2">
       <Image src="/ltm.svg" alt="ltm logo" width={72} height={72} />
       <ul className="items-center flex gap-10">
-        <Link href="/home">Home</Link>
-        <Link href="/home/tentang">Tentang</Link>
-        <Link href="/home/caralapor">Cara Melapor</Link>
-        <Link href="/home/laporan">Laporan Anda</Link>
-        <Link href="/home/kontak">Kontak</Link>
+        <Link className={style.link} href="/home">
+          Home
+        </Link>
+        <Link className={style.link} href="/home/tentang">
+          Tentang
+        </Link>
+        <Link className={style.link} href="/home/caralapor">
+          Cara Melapor
+        </Link>
+        <Link className={style.link} href="/home/laporan">
+          Laporan Anda
+        </Link>
+        <Link className={style.link} href="/home/kontak">
+          Kontak
+        </Link>
         <UserButton />
       </ul>
     </div>
