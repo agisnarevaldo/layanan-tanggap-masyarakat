d0c730175a692c4982e897ff7ffea3e4dac110ca
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-04 07:12:54 +0700

    add a handleSubmit form

--- app/ui/formLapor.tsx
+++ app/ui/formLapor.tsx
@@ -1,11 +1,59 @@
+"use client";
+
 import Link from "next/link";
-import Input from "./input";
-import Label from "./label";
 import Button from "./button";
+import { useState } from "react";
 
 export default function FormLapor() {
+  const [category, setCategory] = useState("");
+  const [waktu, setWaktu] = useState("");
+  const [nama, setNama] = useState("");
+  const [tanggal, setTanggal] = useState("");
+  const [bukti, setBukti] = useState("");
+  const [lokasi, setLokasi] = useState("");
+  const [keterangan, setKeterangan] = useState("");
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    // Kirim data ke API untuk ditambahkan
+    const response = await fetch("/api/create", {
+      method: "POST",
+      body: JSON.stringify({
+        category: category || "",
+        waktu: waktu || "",
+        nama: nama || "",
+        tanggal: tanggal || "",
+        bukti: bukti || "",
+        lokasi: lokasi || "",
+        keterangan: keterangan || "",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      // Data berhasil ditambahkan
+      const data = await response.json();
+      console.log(data);
+
+      // Reset form
+      setCategory("");
+      setWaktu("");
+      setNama("");
+      setTanggal("");
+      setBukti("");
+      setLokasi("");
+      setKeterangan("");
+    } else {
+      // Terjadi kesalahan saat menambahkan data
+      console.error("Gagal menambahkan data laporan");
+    }
+  };
+
   return (
-    <form action="#">
+    <form onSubmit={handleSubmit}>
       <div className="grid gap-4 sm:grid-cols-2 sm:gap-4">
         <div>
           <label
@@ -16,9 +64,13 @@ export default function FormLapor() {
           </label>
           <select
             id="kategori"
+            value={category}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+            onChange={(event) => setCategory(event.target.value)}
           >
-            <option selected={true}>Pilih Laporan Anda</option>
+            <option value="" disabled>
+              Pilih Laporan Anda
+            </option>
             <option value="kesehatan">Kesehatan</option>
             <option value="bencana alam">Bencana Alam</option>
             <option value="Kebakaran">Kebakaran</option>
@@ -27,7 +79,7 @@ export default function FormLapor() {
         </div>
         <div>
           <label
-            htmlFor="name"
+            htmlFor="waktu"
             className="block mb-2 text-sm font-medium text-gray-900"
           >
             Waktu
@@ -36,6 +88,8 @@ export default function FormLapor() {
             type="time"
             name="waktu"
             id="waktu"
+            value={waktu}
+            onChange={(event) => setWaktu(event.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
             placeholder="Type product name"
             required
@@ -52,6 +106,8 @@ export default function FormLapor() {
             type="text"
             name="nama"
             id="nama"
+            value={nama}
+            onChange={(event) => setNama(event.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
             placeholder="Product brand"
             required
@@ -68,6 +124,8 @@ export default function FormLapor() {
             type="date"
             name="tanggal"
             id="tanggal"
+            value={tanggal}
+            onChange={(event) => setTanggal(event.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
             placeholder="$2999"
             required
@@ -85,6 +143,8 @@ export default function FormLapor() {
             type="file"
             name="bukti"
             id="bukti"
+            value={bukti}
+            onChange={(event) => setBukti(event.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
             placeholder="Upload bukti laporan"
           />
@@ -101,6 +161,8 @@ export default function FormLapor() {
             type="url"
             name="lokasi"
             id="lokasi"
+            value={lokasi}
+            onChange={(event) => setLokasi(event.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
             placeholder="Lokasi Kejadian"
             required
@@ -117,9 +179,11 @@ export default function FormLapor() {
           <textarea
             id="keterangan"
             rows={8}
+            name="keterangan"
+            value={keterangan}
+            onChange={(event) => setKeterangan(event.target.value)}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
             placeholder="Tulis Keterangan Anda"
-            defaultValue={""}
           />
         </div>
       </div>
