cba7c9c6ceae0408b9dbcccdcac71d72ef2fd077
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-04 07:10:43 +0700

    create laporan pages && fetch data

--- app/home/laporan/page.tsx
+++ app/home/laporan/page.tsx
@@ -1,7 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const LaporList = () => {
+  const [lapor, setLapor] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api/read");
+      const data = await response.json();
+      setLapor(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="max-w-2xl w-full mx-auto p-8 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-4">Daftar Laporan</h1>
+      <ul className="divide-y divide-gray-200">
+        {lapor.map((item: any) => (
+          <li key={item.id} className="py-4">
+            <div className="flex items-center space-x-4">
+              <div className="flex-1 min-w-0">
+                <p className="text-sm font-medium text-gray-900 truncate">
+                  {item.category}
+                </p>
+                <p className="text-sm text-gray-500 truncate">{item.Waktu}</p>
+                <p className="text-sm text-gray-500 truncate">{item.tanggal}</p>
+              </div>
+              <div>
+                <p className="text-sm text-gray-500 truncate text-center py-2">
+                  {item.status ? "Selesai" : "Proses"}
+                </p>
+                <div className="flex gap-2">
+                  <Link
+                    href={`/home/laporan/edit/${item.id}`}
+                    className="text-center bg-blue-500 py-1 px-2 w-[72px] text-white rounded-md"
+                  >
+                    Edit
+                  </Link>
+                  <button className="bg-red-500 py-1 px-2 w-[72px] text-white rounded-md">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const LaporanPage = () => {
   return (
     <main>
-      <h1>Laporan</h1>
+      <LaporList />
     </main>
   );
 };
