41b848c97b4296f061e53bfd9e680ac266fdca35
Masgenta <gentarailfans@gmail.com>
2024-06-14 09:48:01 +0700

    create admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,30 @@
-export default function AdminPage() {
+import Card from "../ui/admin/card";
+import CardName from "../ui/admin/card-name";
+import CardProfile from "../ui/admin/card-profile";
+
+export default async function AdminPage() {
     return (
-        <main>
-            <h1>Halaman Admin</h1>
-        </main>
+        
+        <div className="flex justify-evenly w-full">
+            <div className="p-5 text-white">
+                <div className="mb-5">
+                    <CardName />
+                </div>
+            
+
+                <div className="pt-5 grid grid-cols-2 gap-x-4 gap-y-4">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+
+            </div>
+
+            <div className="p-5">
+                <CardProfile />
+            </div>
+
+        </div>
     )
 }
