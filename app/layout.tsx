e09ddeb946f52283f47abae4160520752c69d143
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-17 17:12:19 +0700

    chore: Add Providers component to app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
@@ -26,7 +27,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={poppins.className}>{children}</body>
+        <body className={poppins.className}>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
