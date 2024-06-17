cdc44a083641170f0d23ccc26db0f9e16e29b6f7
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-17 22:02:57 +0700

    Refactor formLapor component, improve form submission and enable lazy loading for images

--- app/home/laporan/detail/[id]/page.tsx
+++ app/home/laporan/detail/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -7,45 +9,52 @@ export default function DetailPage({ params }: any) {
   const { id } = params;
   const [lapor, setLapor] = useState([]);
 
-  const [category, setCategory] = useState("");
-  const [waktu, setWaktu] = useState("");
-  const [nama, setNama] = useState("");
-  const [tanggal, setTanggal] = useState("");
-  const [bukti, setBukti] = useState("");
-  const [lokasi, setLokasi] = useState("");
-  const [keterangan, setKeterangan] = useState("");
-
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`/api/reports/${id}`);
       const data = await response.json();
       console.log(data);
       setLapor(data);
-      setCategory(data.category);
-      setWaktu(data.waktu);
-      setNama(data.nama);
-      setBukti(data.bukti);
-      setLokasi(data.lokasi);
-      setKeterangan(data.keterangan);
-      setTanggal(data.tanggal);
     };
 
     fetchData();
   }, [id]);
   return (
-    <main className="mb-32 px-4">
+    <main className="mb-28 px-4">
       <ul className="divide-y divide-gray-200">
         {lapor.map((item: any) => (
             <li
                 key={item.id}
-                className="grid grid-cols-3 bg-gray-100 shadow-lg w-max rounded-2xl border-[3px] px-11 py-6 border-[#436850] mx-auto gap-y-4 gap-x-20 items-center"
+                className="grid grid-cols-3 bg-gray-100 shadow-lg max-w-3xl rounded-2xl border-[3px] px-11 py-6 border-[#436850] mx-auto gap-y-4 gap-x-20 items-start"
                 // style={{ gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))" }}
             >
-              <h1 className="text-3xl font-semibold mb-2 col-span-full">
-                Data Laporan
-                <span className="block text-sm font-light">Yang bersangkutan di bawah ini:</span>
-              </h1>
+              <div className=" mb-2 col-span-full flex justify-between">
+                <div>
+                  <h2 className="text-3xl font-semibold">Data Laporan</h2>
+                  <p className="block text-sm font-light">Yang bersangkutan di bawah ini:</p>
+                </div>
+                <Link href="/home/laporan" >
+                  <Icon icon="ic:round-close" className="text-3xl text-red-600 font-bold hover:text-red-800 rounded-full"  />
+                </Link>
+              </div>
+              <div className="row-span-2">
+                {/* <p className="font-light">
+                  Bukti:{" "}
+                </p> */}
+                {/* <img src={item.bukti} alt="Bukti Kejadian" className="w-full h-auto" /> */}
+                <Image src={item.bukti} alt="Bukti Kejadian" width={200} height={200} className="rounded-lg" />
+              </div>
+
               <div>
+                <p className="font-light">
+                  Nama Pelapor:{" "}
+                </p>
+                <p className="font-semibold text-gray-900 truncate">
+                  {item.nama}
+                </p>
+              </div>
+
+              <div className="">
                 <p className="font-light">
                   Kategori:{" "}
                 </p>
@@ -63,7 +72,7 @@ export default function DetailPage({ params }: any) {
                 </p>
               </div>
 
-              <div>
+              <div className="">
                 <p className="font-light">
                   Lokasi:{" "}
                 </p>
@@ -74,10 +83,10 @@ export default function DetailPage({ params }: any) {
 
               <div>
                 <p className="font-light">
-                  Bukti:{" "}
+                  Status:{" "}
                 </p>
-                <p className="font-semibold text-gray-900 truncate">
-                  {item.bukti}
+                <p className={`text-secondary w-max px-4 text-center font-semibold rounded ${item.status ? "bg-green-700" : "bg-red-600"} truncate`}>
+                  {item.status ? "Selesai" : "Proses"}
                 </p>
               </div>
 
@@ -90,21 +99,15 @@ export default function DetailPage({ params }: any) {
                 </p>
               </div>
 
-              <div>
-                <p className={`text-secondary text-center font-semibold rounded ${item.status ? "bg-green-700" : "bg-red-600"} truncate`}>
-                  {item.status ? "Selesai" : "Proses"}
-                </p>
-              </div>
-
-              <div className="col-span-2 flex justify-between items-center">
-                <div>
+              <div className="">
+                {/* <div> */}
                   <p className="font-light">
                     Keterangan:{" "}
                   </p>
-                  <p className="font-semibold text-gray-900 truncate">
+                  <p className="font-semibold text-gray-900">
                     {item.keterangan}
                   </p>
-                </div>
+                {/* </div> */}
 
               </div>
             </li>
@@ -112,12 +115,12 @@ export default function DetailPage({ params }: any) {
       </ul>
 
       {/* Link kembali */}
-      <Link
+      {/* <Link
           href="/home/laporan"
           className="bg-primary text-white px-6 py-2 rounded-lg absolute bottom-8 right-1/2 translate-x-1/2"
       >
         Kembali
-      </Link>
+      </Link> */}
     </main>
   );
 }
