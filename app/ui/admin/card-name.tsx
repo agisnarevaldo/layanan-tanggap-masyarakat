f67f67388f19e7b2892c85ca7a784337634c7401
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-19 11:00:30 +0700

    refactor: Update CardName component to include username prop

--- app/ui/admin/card-name.tsx
+++ app/ui/admin/card-name.tsx
@@ -1,10 +1,15 @@
 import { Icon } from "@iconify/react";
 
-export default function CardName() {
+interface CardNameProps {
+  username: string;
+}
+
+
+export default function CardName( { username }: CardNameProps) {
   return (
     <div className="bg-green-900 w-auto h-48 p-4 rounded-3xl flex justify-between items-center">
       <div className="flex flex-col pl-5">
-        <h2 className="text-4xl font-semibold">Hello Admin!</h2>
+        <h2 className="text-4xl font-semibold">Hello {username}</h2>
         <p>Semoga harimu menyenangkan</p>
       </div>
 
