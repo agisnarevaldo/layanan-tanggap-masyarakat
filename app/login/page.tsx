6827d765aaaf41c29ed73c2168342b1ec482ae38
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-19 11:00:11 +0700

    refactor: Update login page with lock icon and styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,13 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import FormLogin from "../ui/formLogin";
 
 export default function LoginAdmin() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="min-h-screen flex flex-col items-center justify-center gap-3">
+            <div className="flex items-center gap-3">
+                <h1 className="text-3xl text-gray-900 font-bold">LOGIN</h1>
+                <Icon icon="raphael:lock" className="text-2xl text-gray-900" />
+            </div>
             <FormLogin />
         </div>
     );
