2cd6006ecbae44a9d2117877338045623838faf6
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-24 22:53:25 +0700

    refactor: Update layout component to include overflow-y-hidden

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import AdminSidebar from "../ui/admin/sidebar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen flex overflow-y-hidden">
       <AdminSidebar />
       {children}
     </div>
