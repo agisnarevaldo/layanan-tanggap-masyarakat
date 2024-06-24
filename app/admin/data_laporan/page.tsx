38f377c1161169c2534765b4a489cb07357b93d4
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-24 22:54:23 +0700

    refactor: Remove unnecessary border from DataLaporan component

--- app/admin/data_laporan/page.tsx
+++ app/admin/data_laporan/page.tsx
@@ -1,9 +1,8 @@
 import DataViewer from "@/app/ui/admin/dataViewer";
-import Tabel from "../../ui/admin/tabel";
 
 export default async function DataLaporan() {
   return (
-    <div className="w-full m-10 border-green-700 border-2 rounded-3xl">
+    <div className="w-full m-10 rounded-3xl">
       <h1 className="text-2xl font-bold text-center">Data Laporan Masuk</h1>
       <DataViewer />
     </div>
