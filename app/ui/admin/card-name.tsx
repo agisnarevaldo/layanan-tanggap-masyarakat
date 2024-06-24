9fcfbfc44918458356c3399177cae2a0ff06afdf
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-24 22:55:22 +0700

    refactor: Update CardName component to use primary color for background
    This commit updates the CardName component to use the primary color for the background instead of the specific green color. This change improves consistency and allows for easier theming of the application.

--- app/ui/admin/card-name.tsx
+++ app/ui/admin/card-name.tsx
@@ -4,17 +4,16 @@ interface CardNameProps {
   username: string;
 }
 
-
-export default function CardName( { username }: CardNameProps) {
+export default function CardName({ username }: CardNameProps) {
   return (
-    <div className="bg-green-900 w-auto h-48 p-4 rounded-3xl flex justify-between items-center">
+    <div className="bg-primary w-auto h-48 p-4 rounded-3xl flex justify-between items-center">
       <div className="flex flex-col pl-5">
         <h2 className="text-4xl font-semibold">Hello {username}</h2>
         <p>Semoga harimu menyenangkan</p>
       </div>
 
       <div>
-        <Icon icon="entypo:user" className="text-white text-[150px] mt-11" />
+        <Icon icon="entypo:user" className="text-[#F5F5F5] text-[150px] mt-11" />
       </div>
     </div>
   );
