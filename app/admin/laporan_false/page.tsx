b1eff1a3cc5060b9cf99da6bb79997c1eedb8dae
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-24 22:54:56 +0700

    feat: Add page for displaying completed reports
    This commit adds a new page component, `Page`, for displaying completed reports. The page includes a title and a component called `DataFalse` that renders the data for completed reports. This addition enhances the application by providing a dedicated page for viewing completed reports.
    
    Refactor: Remove unnecessary border from DataLaporan component
    Refactor: Update AdminPage component to fetch and display data from API
    Refactor: Ignore .idea/ directory in .gitignore
    Refactor: Update layout component to include overflow-y-hidden
    Refactor: Update DataViewer component to use primary color for background
    Refactor: Update DataViewer component to use primary color for background
    Feat: Add modal component for editing user profile
    Refactor: Update ModalLogout component to accept custom styling props
    Refactor: Update AdminSidebar component to use primary color and hover effect for menu items
    Chore: Update error message for user not found in unprocessed API endpoint

---
+++ app/admin/laporan_false/page.tsx
@@ -0,0 +1,10 @@
+import DataFalse from "@/app/ui/admin/dataFalse";
+
+export default function Page() {
+    return (
+        <div className="w-full ml-10 mt-10 rounded-3xl">
+            <h1 className="text-2xl font-bold text-center">Data Laporan Selesai</h1>
+            <DataFalse />
+        </div>
+    )
+}
