9c63aced8c7254c996295b6af21851bb43ac8e23
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-24 22:50:07 +0700

    refactor: Update AdminSidebar component to use primary color and hover effect for menu items

--- app/ui/admin/sidebar.tsx
+++ app/ui/admin/sidebar.tsx
@@ -8,7 +8,7 @@ import ModalLogout from "@/app/ui/admin/modalLogout";
 
 const AdminSidebar = () => {
   return (
-    <div className="bg-green-900 text-white w-64 min-h-screen">
+    <div className="bg-primary text-white w-64 min-h-screen">
       <Image
         src="/ltm.svg"
         className="mx-auto mt-5"
@@ -18,40 +18,43 @@ const AdminSidebar = () => {
       />
 
       <ul className="mt-12">
-        <li className="p-4 hover:bg-green-800">
+        <li className="p-4 bg-primary hover:bg-[#4C7D5D] cursor-pointer">
           <Link href="/admin">
             <div className="flex items-center">
               <Icon icon="material-symbols:home" className="mr-4" /> Dashboard
             </div>
           </Link>
         </li>
-        <li className="p-4 hover:bg-green-800 cursor-pointer">
+        <li className="p-4 hover:bg-[#4C7D5D] cursor-pointer">
           <Link href="/admin/data_laporan">
             <div className="flex items-center">
-              <Icon icon="heroicons:user-group-20-solid" className="mr-4" />{" "}
-              Laporan Masuk
+              <Icon icon="heroicons:user-group-20-solid" className="mr-4" />
+              {" "}Laporan Masuk
             </div>
           </Link>
         </li>
-        <li className="p-4 hover:bg-green-800 cursor-pointer">
+        <li className="p-4 hover:bg-[#4C7D5D] cursor-pointer">
+          <Link href="/admin/laporan_false">
+            <div className="flex items-center">
+              <Icon icon="carbon:user-filled" className="mr-4" />
+              {" "}Laporan Proses
+            </div>
+          </Link>
+        </li>
+        <li className="p-4 hover:bg-[#4C7D5D] cursor-pointer">
           <Link href="/admin/laporan_true">
             <div className="flex items-center">
-              <Icon icon="carbon:user-filled" className="mr-4" /> Laporan Selesai
-            </div>
-          </Link>
-        </li>
-        <li className="p-4 hover:bg-green-800 cursor-pointer">
-          <Link href="/settings">
-            <div className="flex items-center">
-              <Icon icon="streamline:task-list-solid" className="mr-4" />{" "}
-              Pengaturan
+              <Icon icon="streamline:task-list-solid" className="mr-4" />
+              {" "}Laporan Selesai
             </div>
           </Link>
         </li>
       </ul>
 
       <div className="flex items-center absolute bottom-0 p-4">
-        <ModalLogout />
+        <ModalLogout bgColor="transparent" textColor="white" width="full">
+          <Icon icon="carbon:logout" className="mr-4" /> Keluar
+        </ModalLogout>
         {/*<button className="flex items-center">*/}
         {/*  <Icon icon="carbon:logout" className="mr-4" /> Logout*/}
         {/*</button>*/}
